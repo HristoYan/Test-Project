9c471654f4233f7b8bbc9a9d2c5dfc21b4788053
HristoYan <hristoyan@abv.bg>
2024-09-19 20:44:55 +0300

    logger and loguru

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+# import logging
 import os
+from loguru import logger
 import requests
 from app_config import UserInfo
+
+# logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(name)s:%(message)s")
+# logger = logging.getLogger(__name__)
 
 
 user = UserInfo()
@@ -9,9 +14,12 @@ api_key = os.getenv(user.get_api())
 
 base_url = 'http://api.weatherapi.com/v1'
 
-response = requests.get(f'{base_url}/current.json?q=Plovdiv&key={user.get_api()}')
+logger.info('Making a get request to the API')
+response = requests.get(f'{base_url}/current.json?q=Plovdiv&key={user.get_api()}1')
 if response.ok:
     print(response.json())
 
 else:
+    if response.status_code == 403:
+        logger.error(f'Check your API key: {response.status_code}')
     print(f'Error: {response.status_code}')
